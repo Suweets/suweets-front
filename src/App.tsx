5578e660bcaf9d5bde62944adaacc53ca11876e5
LucasBened <lucas@bscconnect.com.br>
2025-03-26 09:23:48 -0300

    Ajustando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-export default function App() {
+export function App() {
   return (
-    <div className="flex justify-center items-center bg-blue-500 h-screen">
-      <p>Hello World</p>
+    <div className="flex h-full items-center justify-center bg-gray-100">
+      <div className="rounded-lg bg-white p-4 shadow-lg">
+        <h1 className="text-2xl font-bold">
+          Hello, Vite + React + Tailwind CSS!
+        </h1>
+        <p className="text-gray-500">
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+      </div>
     </div>
   );
 }
