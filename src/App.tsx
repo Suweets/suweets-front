6b7f941cca15b57982b4f79a81a165e1a66b1f9f
lucasbened <lussivbsilva1@gmail.com>
2025-04-03 17:32:14 -0300

    [feat] criando header e configurando o router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
+import router from "./router/router";
+import { RouterProvider } from "react-router";
+
 export function App() {
-  return (
-    <div className="flex h-full items-center justify-center bg-gray-100">
-      <div className="rounded-lg bg-white p-4 shadow-lg">
-        <h1 className="text-2xl font-bold">
-          Hello, Vite + React + Tailwind CSS!
-        </h1>
-        <p className="text-gray-500">
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-      </div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
