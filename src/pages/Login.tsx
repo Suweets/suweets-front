277ea9298ad7220ab3d660470df55764890af0c9
LucasBened <lucas@bscconnect.com.br>
2025-04-23 20:56:50 -0300

    [feat] adicionados serviços de login e registro do usuário

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,18 +2,61 @@ import { Link } from "react-router";
 import Button from "../components/Button/Button";
 import FormInput from "../components/FormInput/FormInput";
 import strawberry from "../assets/strawberry.svg";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userLogin } from "../types/user";
+import { useMutation } from "@tanstack/react-query";
+import { loginUser } from "../services/loginUser";
 
 export default function Login() {
+  const loginUserSchema = z.object({
+    email: z.string().email("Email inválido").min(1, "Email é obrigatório"),
+    password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+  });
+
+  const { register, handleSubmit, reset } = useForm<userLogin>({
+    resolver: zodResolver(loginUserSchema),
+  });
+
+  const loginMutation = useMutation({
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      reset();
+      console.log("User logged in successfully:", data);
+    },
+
+    onError: (error) => {
+      console.error("Error logging in user:", error);
+    },
+  });
+
+  const handleLogin = (data: userLogin) => {
+    loginMutation.mutate(data);
+    console.log("Form data:", data);
+  };
+
   return (
     <div className="bg-light-brown flex flex-col">
       <section className="flex h-screen w-full items-center justify-center p-[70px]">
         <div className="flex h-full w-full items-center justify-center">
-          <form className="bg-cream shadow-grayShadow border-base-gray relative mt-20 flex h-fit w-fit flex-col items-center rounded-3xl border-2 px-16 py-3">
+          <form
+            onSubmit={handleSubmit(handleLogin)}
+            className="bg-cream shadow-grayShadow border-base-gray relative mt-20 flex h-fit w-fit flex-col items-center rounded-3xl border-2 px-16 py-3"
+          >
             <p className="text-chocolate-brown font-bold">Faça seu Login</p>
             <div className="mt-10 mb-10 flex h-full w-fit items-center justify-center gap-24">
               <div className="flex h-full w-full flex-col gap-4">
-                <FormInput label="Email *" required />
-                <FormInput label="Senha *" required />
+                <FormInput
+                  label="Email *"
+                  {...register("email")}
+                  type="email"
+                />
+                <FormInput
+                  label="Senha *"
+                  {...register("password")}
+                  type="password"
+                />
               </div>
             </div>
             <div className="flex w-full flex-col items-center justify-center gap-5">
@@ -22,7 +65,10 @@ export default function Login() {
               </Button>
               <p className="text-xs font-light">
                 Ainda não possui uma conta?{" "}
-                <Link to={"/register"} className="text-chocolate-brown font-bold">
+                <Link
+                  to={"/register"}
+                  className="text-chocolate-brown font-bold"
+                >
                   Crie uma
                 </Link>
               </p>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,26 +2,98 @@ import { Link } from "react-router";
 import Button from "../components/Button/Button";
 import FormInput from "../components/FormInput/FormInput";
 import strawberry from "../assets/strawberry.svg";
+import { useMutation } from "@tanstack/react-query";
+import { createUser } from "../services/createUser";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userRegister } from "../types/user";
 
 export default function Register() {
+  const registerUserSchema = z.object({
+    nome: z.string().min(1, "Nome é obrigatório"),
+    sobrenome: z.string().optional(),
+    email: z.string().email("Email inválido").min(1, "Email é obrigatório"),
+    password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+    cpf: z.string().optional(),
+    telefone: z.string().optional(),
+    funcao: z.number().optional(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<userRegister>({
+    resolver: zodResolver(registerUserSchema),
+  });
+
+  const userMutation = useMutation({
+    mutationFn: createUser,
+    onSuccess: (data) => {
+      console.log("User created successfully:", data);
+      reset();
+    },
+
+    onError: (error) => {
+      console.error("Error creating user:", error);
+    },
+  });
+
+  const handleRegister = (data: userRegister) => {
+    console.log("Form data:", data);
+    userMutation.mutate(data);
+  };
+
   return (
     <div className="bg-light-brown flex flex-col">
       <section className="flex h-screen w-full items-center justify-center p-[70px]">
         <div className="flex h-full w-full items-center justify-center">
-          <form className="bg-cream shadow-grayShadow border-base-gray relative mt-20 flex h-fit w-[700px] flex-col items-center rounded-3xl border-2 px-14 py-3">
+          <form
+            onSubmit={handleSubmit(handleRegister)}
+            className="bg-cream shadow-grayShadow border-base-gray relative mt-20 flex h-fit w-[700px] flex-col items-center rounded-3xl border-2 px-14 py-3"
+          >
             <p className="text-chocolate-brown font-bold">Faça seu Registro</p>
             <div className="mt-10 mb-10 flex h-full w-full items-start justify-between gap-24">
               <div className="flex h-full w-1/2 flex-col gap-4">
-                <FormInput label="Nome *" required />
-                <FormInput label="Sobrenome" />
-                <FormInput label="Senha *" type="password" required />
-                <FormInput label="Confirmar Senha *" type="password" required />
+                <FormInput label="Nome *" {...register("nome")} />
+                {errors.nome && (
+                  <p className="text-xs text-red-500">{errors.nome.message}</p>
+                )}
+                <FormInput label="Sobrenome" {...register("sobrenome")} />
+                <FormInput
+                  label="Senha *"
+                  type="password"
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="text-xs text-red-500">
+                    {errors.password.message}
+                  </p>
+                )}
+                <FormInput label="Confirmar Senha *" type="password" />
               </div>
               <div className="flex h-full w-1/2 flex-col gap-4">
-                <FormInput label="Email *" type="email" required />
-                <FormInput label="Confirmar Email *" type="email" required />
-                <FormInput label="CPF *" required />
-                <FormInput label="Telefone" type="tel" />
+                <FormInput
+                  label="Email *"
+                  type="email"
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <p className="text-xs text-red-500">{errors.email.message}</p>
+                )}
+                <FormInput label="Confirmar Email *" type="email" />
+                {errors.email && (
+                  <p className="text-xs text-red-500">{errors.email.message}</p>
+                )}
+                <FormInput label="CPF" {...register("cpf")} />
+
+                <FormInput
+                  label="Telefone"
+                  type="tel"
+                  {...register("telefone")}
+                />
               </div>
             </div>
             <div className="flex w-1/2 flex-col items-center justify-center gap-5">
